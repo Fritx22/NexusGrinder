ef2b6a2be2d3bbce5cd6bc7a78d4f70ea10f2870
Fritx22 <57330546+Fritx22@users.noreply.github.com>
2020-02-19 17:41:49 -0300

    Added TODO

--- src/main/java/net/coasterman10/Annihilation/object/Team.java
+++ src/main/java/net/coasterman10/Annihilation/object/Team.java
@@ -1,6 +1,9 @@
 package net.coasterman10.Annihilation.object;
 
 import com.sun.istack.internal.NotNull;
+import net.coasterman10.Annihilation.Annihilation;
+import net.coasterman10.Annihilation.Util;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
@@ -8,12 +11,16 @@ import java.util.UUID;
 
 public final class Team {
     private final TeamEnum teamColor;
+    //private final Nexus nexus;
     private final HashMap<Player, UUID> players;
     private static byte nextId = 0;
     private final byte id;
 
     public Team(@NotNull TeamEnum color) {
         this.teamColor = color;
+        // TODO: Create the game class
+        //Location nexusLocation = Util.parseLocation(Annihilation.getInstance().getConfig().getString("nexuses." + this.teamColor.name().toLowerCase()));
+        //this.nexus = new Nexus(teamColor, nexusLocation, 75);
         this.id = nextId++;
         this.players = new HashMap<Player, UUID>();
     }
